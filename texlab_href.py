d463c9dfa1a1de52c4f9f23b04617edfa98004a1
fouche <tetrapharmakon@gmail.com>
2018-12-01 00:57:44 +0100

    non  capisco perché non funzioni

--- texlab_href.py
+++ texlab_href.py
@@ -1,10 +1,14 @@
 import re
 
-REGEX = r"\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2}"
-SUB = r"\\LabRef[\3]{\1}{\2}"
+REGEXES = [(r"\\emph\{(\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2})\}", r"\1"),
+           (r"\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2}", r"\\LabRef[\3]{\1}{\2}"),
+           (r"\n{2,}", r"\n"),
+           (r"\n*\$(?:\\[,.;:])*\$\n*", r"\n")]
 INFILE = open('to-be-tidied.tex', 'r')
 outfile = open('tidied.tex', 'w')
 
 for line in INFILE:
-    newl = re.sub(REGEX, SUB, line)
+    for rex, sub in REGEXES:
+        newl = re.sub(rex, sub, line)
+    print(newl)
     outfile.write(newl)

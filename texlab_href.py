8a885de9bf73fe8625c143b5055413d827a3cb18
fouche <tetrapharmakon@gmail.com>
2018-12-01 10:26:59 +0100

    adesso texlab_href funziona

--- texlab_href.py
+++ texlab_href.py
@@ -1,14 +1,17 @@
 import re
 
-REGEXES = [(r"\\emph\{(\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2})\}", r"\1"),
+REGEXES = [(r"\\emph{(\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2})}", r"\1"),
+           # \emph{[[foo:bar|baz]]} -> [[foo:bar|baz]]
            (r"\[{2}(\w+)\:(.*?)(?:\|(.*?)?)?]{2}", r"\\LabRef[\3]{\1}{\2}"),
-           (r"\n{2,}", r"\n"),
+           # [[foo:bar|baz]] -> \LabRef[baz]{foo}{bar}
+           (r"\n{3,}", r"\n\n"),
+           # remove iterated newlines
            (r"\n*\$(?:\\[,.;:])*\$\n*", r"\n")]
+        #    remove $\,\,\,\,\,\,$
 INFILE = open('to-be-tidied.tex', 'r')
 outfile = open('tidied.tex', 'w')
 
 for line in INFILE:
     for rex, sub in REGEXES:
-        newl = re.sub(rex, sub, line)
-    print(newl)
-    outfile.write(newl)
+        line = re.sub(rex, sub, line)
+    outfile.write(line)
